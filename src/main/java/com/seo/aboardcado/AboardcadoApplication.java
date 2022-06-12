5af7e4c159204b3248b198c87c30e3eac8ff51b0
devkwonms <alstj1379@nate.com>
2022-06-12 18:08:33 +0900

    Initial commit

---
+++ src/main/java/com/seo/aboardcado/AboardcadoApplication.java
@@ -0,0 +1,13 @@
+package com.seo.aboardcado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboardcadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboardcadoApplication.class, args);
+	}
+
+}
