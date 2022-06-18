6486f307bbb065e323e81771d7a5a6dd384e475e
devkwonms <alstj1379@nate.com>
2022-06-18 23:59:42 +0900

    BoardList , Paging(Error)

--- src/main/java/com/seo/aboardcado/controller/BoardController.java
+++ src/main/java/com/seo/aboardcado/controller/BoardController.java
@@ -1,11 +1,20 @@
 package com.seo.aboardcado.controller;
 
+import com.seo.aboardcado.domain.entity.Board;
+import com.seo.aboardcado.domain.repository.BoardRepository;
 import com.seo.aboardcado.dto.BoardDto;
 import com.seo.aboardcado.service.BoardService;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -13,14 +22,68 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class BoardController {
 
     private BoardService boardService;
+    private BoardRepository boardRepository;
 
     @GetMapping("/post")
     public String post(){
         return "board/post";
     }
+
     @PostMapping("/post")
     public String write(BoardDto boardDto){
         boardService.savePost(boardDto);
-        return "redirect:/post";
+        return "redirect:/";
     }
+
+    @GetMapping("/")
+    public String list(Model model){
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("postList", boardDtoList);
+        return "board/list";
+    }
+
+    @GetMapping("/post/{id}")
+        public String detail(@PathVariable("id") Long id, Model model){
+            BoardDto boardDto = boardService.postDtl(id);
+            model.addAttribute("post", boardDto);
+            return "board/detail";
+    }
+
+    @GetMapping("/post/edit/{id}")
+        public String edit(@PathVariable("id") Long id, Model model){
+            BoardDto boardDto = boardService.postDtl(id);
+            model.addAttribute("post", boardDto);
+            return "board/edit";
+    }
+    @PutMapping("/post/edit/{id}")
+        public String update(BoardDto boardDto){
+            boardService.savePost(boardDto);
+            return "redirect:/post/{id}";
+    }
+    @DeleteMapping("/post/{id}")
+        public String delete(@PathVariable("id") Long id){
+            boardService.deletePost(id);
+            return "redirect:/";
+    }
+    @GetMapping("/")
+        public String boardList(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, Model model){
+        Page<Board> page = boardRepository.findAll(pageable);
+        List<BoardDto> boardList = boardService.getBoardList(pageable);
+
+        int nowPage = page.getPageable().getPageNumber()+1;
+        int startPage = Math.max(nowPage-4,1);
+        int endPage =   Math.min(nowPage+5, page.getTotalPages());
+        int totalPages = page.getTotalPages();
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("totalPages", totalPages);
+
+        return "board/list";
+
+
+    }
+
 }
--- src/main/java/com/seo/aboardcado/service/BoardService.java
+++ src/main/java/com/seo/aboardcado/service/BoardService.java
@@ -1,24 +1,61 @@
 package com.seo.aboardcado.service;
 
+import com.seo.aboardcado.domain.entity.Board;
 import com.seo.aboardcado.domain.repository.BoardRepository;
 import com.seo.aboardcado.dto.BoardDto;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Service
+@Transactional
 public class BoardService {
 
     private BoardRepository boardRepository;
 
-    @Transactional
-    public Long savePost(BoardDto boardDto){
+    public Long savePost(BoardDto boardDto) {
 
         return boardRepository.save(boardDto.toEntity()).getId();
     }
 
+    private BoardDto toDto(Board board){
+        return BoardDto.builder()
+                .id(board.getId())
+                .author(board.getAuthor())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .createdDate(board.getCreatedDate())
+                .build();
+    }
+
+    public List<BoardDto> getBoardList(Pageable pageable){
+        Page<Board> boards = boardRepository.findAll(pageable);
+        List<BoardDto> boardList = new ArrayList<>();
+
+        for (Board board : boards){
+            boardList.add(this.toDto(board));
+        }
+        return boardList;
+    }
+
+    public BoardDto postDtl(Long id) {
+        Board board = boardRepository.findById(id).get();
+
+        BoardDto boardDto = this.toDto(board);
+        return boardDto;
+    }
+
+    public void deletePost(Long id){
+        boardRepository.deleteById(id);
+    }
 
 
 }
